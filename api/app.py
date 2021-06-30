e7052cbcbd52dc1b5e9f7a168cb07e6d435d26f7
Charlotte Suaud <charlotte.suaud@gmail.com>
2021-06-30 17:53:25 +0200

    Made predict endpoint

--- api/app.py
+++ api/app.py
@@ -1,5 +1,7 @@
 
 from fastapi import FastAPI
+from joblib import load
+import pandas as pd
 
 app = FastAPI()
 
@@ -11,3 +13,37 @@ def index():
 
 
 # Implement a /predict endpoint
+@app.get("predict")
+def predict(original_title, title, release_date, duration_min, 
+            description, budget, original_language, status, 
+            number_of_awards_won, number_of_nominations, has_collection,
+            all_genres, top_countries, number_of_top_productions, available_in_english):
+    eng_dict = {
+        'True': True,
+        'False': False
+    }
+    X_dict = {
+        'original_title': original_title,
+        'title': title,
+        'release_date': release_date,
+        'duration_time': float(duration_min),
+        'description': description,
+        'budget': float(budget), 
+        'original_language': original_language, 
+        'status': status, 
+        'number_of_awards_won': int(number_of_awards_won), 
+        'number_of_nominations': int(number_of_nominations), 
+        'has_collection': int(has_collection),
+        'all_genres': all_genres,
+        'top_countries': top_countries,
+        'number_of_top_productions': float(number_of_top_productions),
+        'available_in_english': eng_dict[available_in_english]
+    }
+    model = load('model.joblib')
+    X_true = pd.DataFrame.from_dict(X_dict)
+    popularity = model.predict(X_true)[0]
+    return {
+        'title': title,
+        'popularity': popularity
+        }
+
